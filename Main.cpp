20fe8412f4bca151f858bdc2e43a2f7a8831aa7c
Carson W <159509119+Cwyatt3@users.noreply.github.com>
2024-02-09 12:51:14 -0600

    Update to Port Scanner.cpp
    Updated to add additional functionality, such as:
    * Option to scan every IPv4 port available at an IPv4 address.

--- Main.cpp
+++ Main.cpp
@@ -9,20 +9,45 @@ static bool port_is_open(const std::string &address, int port)
 
 int main()
 {
-    std::string address; 
+    std::string address,
+                choice;
+
     int port;
     
-    std::cout << "Enter the IPv4 address: \n"; // Get the IPv4 address.
-    std::getline(std::cin, address); 
-
-    std::cout << "Enter the port at the address: \n"; // Get the port.
-    std::cin >> port;
     
-    std::cout << "Scanning " << port << " at " << address << ": "; // Scan it and return Open or Closed.
+    std::cout << "Would you like to scan a specific port or all available (answer with specific or all)? \n";
+    std::getline(std::cin, choice);
+    
+    if (choice == "specific") {
 
-    if (port_is_open(address, 80))
-        std::cout << "OPEN" << std::endl;
-    else
-        std::cout << "CLOSED" << std::endl;
+        std::cout << "Enter the IPv4 address: \n"; // Get the IPv4 address.
+        std::getline(std::cin, address);
+
+        std::cout << "Enter the port at the address: \n"; // Get the port.
+        std::cin >> port;
+
+        std::cout << "Scanning " << port << " at " << address << ": "; // Scan it with TCP protocol, and return Open or Closed.
+
+        if (port_is_open(address, port))
+            std::cout << "OPEN" << std::endl;
+        else
+            std::cout << "CLOSED" << std::endl;
+    }
+    else if (choice == "all") {
+        
+        std::cout << "Enter the IPv4 address: \n";
+        std::getline(std::cin, address);
+
+        for (long count = 0; count <= 65535; count++) { // Scans every possible IPv4 using TCP protocol.
+
+            if (port_is_open(address, count))
+                std::cout << "Port " << count << ": OPEN" << std::endl;
+            else
+                std::cout << "Port " << count << ": CLOSED" << std::endl;
+
+        }
+
+    }
+
     return 0;
 }
